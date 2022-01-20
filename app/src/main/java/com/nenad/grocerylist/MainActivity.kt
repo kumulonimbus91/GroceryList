a20ae6d07041b872e3d3d88aa7ee55fcecf02fbc
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-01-20 01:35:35 +0100

    custom dialog, end of project.

--- app/src/main/java/com/nenad/grocerylist/MainActivity.kt
+++ app/src/main/java/com/nenad/grocerylist/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.nenad.grocerylist
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -40,19 +44,62 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.ItemClickInterface
         val factory = GroceryListViewModelFactory(groceryRepository)
 
         viewModel = ViewModelProvider(this, factory).get(GroceryListViewModel::class.java)
-        viewModel.getAllGroceryItems().observe(this, Observer {
+        viewModel.getAllGroceryItems().observe(this, {
             recyclerViewAdapter.list = it
             recyclerViewAdapter.notifyDataSetChanged()
         })
 
         addFab.setOnClickListener {
-
+         openDialog()
         }
 
 
     }
 
-    override fun onItemClicked(groceryListItem: GroceryListItem) {
+    fun openDialog() {
 
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.add_dialog)
+        val cancelBtn = dialog.findViewById<AppCompatButton>(R.id.btnCancel)
+        val addBtn = dialog.findViewById<AppCompatButton>(R.id.add_btn)
+        val itemId = dialog.findViewById<EditText>(R.id.edit_text_id)
+        val itemPrice = dialog.findViewById<EditText>(R.id.edit_text_price)
+        val itemAmount = dialog.findViewById<EditText>(R.id.tv_tc_amount)
+
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        addBtn.setOnClickListener {
+            val itemName: String = itemId.text.toString()
+            val itemPr: String = itemPrice.text.toString()
+            val itemAm: String = itemAmount.text.toString()
+
+            val qty: Int = itemAm.toInt()
+            val pr: Int = itemPr.toInt()
+            if (itemName.isNotEmpty() && itemPr.isNotEmpty() && itemAm.isNotEmpty()) {
+
+                val item = GroceryListItem(itemName, qty, pr)
+
+                viewModel.insert(item)
+
+                Toast.makeText(applicationContext, "Item inserted in the list", Toast.LENGTH_SHORT).show()
+
+                recyclerViewAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+
+            } else {
+                Toast.makeText(this, "Please enter valid values", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dialog.show()
+
+
+    }
+
+    override fun onItemClicked(groceryListItem: GroceryListItem) {
+       viewModel.delete(groceryListItem)
+        recyclerViewAdapter.notifyDataSetChanged()
+        Toast.makeText(this, "Item deleted", Toast.LENGTH_SHORT).show()
     }
 }
